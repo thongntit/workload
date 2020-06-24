e8fc27609db1f32de024d2d049070ac5e8a005ef
Thong Nguyen <thong.nt@teko.vn>
2020-06-24 15:45:25 +0700

    Add services stack

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,7 @@
-docker stack deploy --compose-file ./stacks/todo-app/docker-compose.yml todo-stack
+# Stack traefik
+docker-compose -f stacks/traefik/docker-compose.yml pull
+docker stack deploy --compose-file stacks/traefik/docker-compose.yml traefik
+
+# Stack todo-app
+docker-compose -f stacks/todo-app/docker-compose.yml pull
+docker stack deploy --compose-file stacks/todo-app/docker-compose.yml todo
