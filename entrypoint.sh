bdfe092a8d3b12d2842e4ee96e0407b355921764
Thong Nguyen <nguyentrthong95@gmail.com>
2021-02-10 16:45:02 +0700

    :zap: add database

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,10 @@
 docker-compose -f stacks/traefik/docker-compose.yml pull
 docker stack deploy --compose-file stacks/traefik/docker-compose.yml traefik
 
+# Database
+docker-compose -f stacks/database/docker-compose.yml pull
+docker stack deploy --compose-file stacks/database/docker-compose.yml database
+
 # Stack todo-app
 docker-compose -f stacks/todo-app/docker-compose.yml pull
 docker stack deploy --compose-file stacks/todo-app/docker-compose.yml todo
